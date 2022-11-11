cb4a0a891c5368824bea7c072147e4292fee4cc7
Joseph Weller <joseph5062a@gmail.com>
2022-11-10 22:20:33 -0600

    foundation post endpoint

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,19 +20,26 @@ type Artist struct {
 }
 
 //TODO
-// 1. Create hashmap of artists
+// 1. When post endpoint is called, create hashmap of artists
 // 2. for each artist start a goroutine, in the routine: Retrieve all albums w/ Query 'include_groups':'appears_on'
 // 3. check if any artists in ownership of album are included in artist hashmap
 // 4. if yes, iterate through album to find song that contains collaberation between both artists and append to return list
 // 5. concatinate return lists from all goroutines into single list, ensuring to remove duplicates
 // 6. return single list to frontend
 
+func getCollabs(c *gin.Context) {
+	body := c.Request.Body
+	artists, err := ioutil.ReadAll(body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "")
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": string(artists),
+	})
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/collabs", getCollabs)
 	r.Run()
 }
