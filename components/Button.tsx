94aa622f915fa216b69c3c69ab9f611a5c69779f
Joseph Weller <joseph5062a@gmail.com>
2022-11-23 19:14:25 -0600

    Following page redesign

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,22 @@
+import Tooltip from "./Tooltip";
+
 type Props = {
     onClick: Function,
     size: string,
-    children: string
+    children: string,
+    tooltip?: string
 };
 
-export default function Button({ onClick, size, children }: Props) {
-    return size === "lg" ? <div className="h-12 w-80 border-green border-solid border-2 rounded-3xl align-middle hover:bg-green hover:cursor-pointer group" onClick={e => onClick()}>
+export default function Button({ onClick, size, children, tooltip }: Props) {
+    const button = size === "lg" ? <div className="h-12 w-80 border-green border-solid border-2 rounded-3xl align-middle hover:bg-green hover:cursor-pointer group" onClick={e => onClick()}>
         <div className="text-white font-bold text-xl text-center h-12 top-1.5 relative group-hover:text-black">{children}</div>
     </div> :
     <div className="h-10 w-32 border-green border-solid border-2 rounded-3xl align-middle hover:bg-green hover:cursor-pointer group" onClick={e => onClick()}>
         <div className="text-white font-bold text-md text-center h-12 top-1 relative group-hover:text-black">{children}</div>
     </div>;
+    return tooltip ? <Tooltip text={tooltip}>
+        {button}
+    </Tooltip> : 
+    button;
 }
+
---
+++ components/Tooltip.tsx
@@ -0,0 +1,21 @@
+import React, { memo } from "react";
+
+export type TooltipProps = {
+  children: React.ReactNode;
+  text: string;
+};
+
+const Tooltip: React.FC<TooltipProps> = memo((props) => {
+  return (
+    <span className="group relative">
+      <span className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-med-grey px-2 py-1 text-white opacity-0 transition before:absolute before:left-1/2 before:top-full before:-translate-x-1/2 before:border-4 before:border-transparent before:border-t-med-grey before:border-dark-gray before:content-[''] group-hover:opacity-100">
+        {props.text}
+      </span>
+      {props.children}
+    </span>
+  );
+});
+
+Tooltip.displayName = "Tooltip";
+
+export default Tooltip;
