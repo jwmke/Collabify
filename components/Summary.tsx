f7b80d837fe08fe265902f86857f0a262b5bba1e
Joseph Weller <joseph5062a@gmail.com>
2022-12-16 21:21:58 -0600

    summary collab graph preview

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -2,38 +2,46 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Summary({ collabStats, topCollabArtists, closeModal }: { collabStats: number[], topCollabArtists: { [artists: string]: number }, closeModal:Function }) {
-    const [top5, setTop5] = useState([] as [string, number][]); 
+    const [top5, setTop5] = useState([] as [string, number][]);
+    const [imageUrl, setImageUrl] = useState(null as any);
 
     useEffect(() => {
+        setTimeout(()=>{ // TODO: Work around Image URL size limitations https://stackoverflow.com/questions/695151/data-protocol-url-size-limitations
+            setImageUrl(document.getElementsByTagName("canvas")[0].toDataURL("image/png"));
+        }, 300);
         setTop5(Object.entries(topCollabArtists)
             .sort(([, a],[, b]) => b-a)
             .slice(0,5));
     }, []);
 
     // TODO -> Refine UI
-    return <div className="bg-med-grey rounded-lg fixed text-center w-1/2 h-1/2">
-        <p className="text-white">{`Discovered ${collabStats[0]} collabs from ${collabStats[1]} of your favorite artists.`}</p>
+    return <div className="bg-med-grey rounded-lg fixed text-center w-1/2 h-1/2 text-white top-24">
+        <p className="">{`Discovered ${collabStats[0]} collabs from ${collabStats[1]} of your favorite artists.`}</p>
         <div>
-            {top5.map((artistCollabs) => {
+            {top5.map((artistCollabs, idx) => {
                 const artists = JSON.parse(artistCollabs[0]);
-                return <div className="inline-block">
+                return <div className="flex" key={idx}>
                     <span>
-                        <Image src={artists[0].img} alt={artists[0].name} width={100} height={100} />
+                        <p className="">{`${idx + 1}. `}</p>
+                    </span>
+                    <span>
+                        <Image src={artists[0].img} alt={artists[0].name} width={60} height={60} />
                         {artists[0].name}
                     </span>
                     <span>
-                        <p className="text-white">+</p>
+                        <p className="">+</p>
                     </span>
                     <span>
-                        <Image src={artists[1].img} alt={artists[1].name} width={100} height={100} />
+                        <Image src={artists[1].img} alt={artists[1].name} width={60} height={60} />
                         {artists[1].name}
                     </span>
                     <span>
-                        <p className="text-white">{`: ${artistCollabs[1]}`}</p>
+                        <p className="">{`: ${artistCollabs[1]}`}</p>
                     </span>
                 </div>
             })}
         </div>
+        <Image unoptimized src={imageUrl} alt={"Collab Graph"} width={700} height={400} />
         <button onClick={() => closeModal()}>Close</button>
     </div>; 
 }
